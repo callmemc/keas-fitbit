a955073ad6b32bd8583dafc1bbe07f5e75ca0380
Adam Bosworth <adam.bosworth@keas.com>
2012-05-07 08:54:14 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitBit::Application.config.secret_token = '5a24a6c780a3eddf1a43a3a6e940251e08edbbdb33c8f1cf2ea56e22969a322809f277b52cfe60bb00f7bacb188595a68d12a53bbb97b94174c6ecb8b7b7b998'
