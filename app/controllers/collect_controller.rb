188288e12782af213f4cde483992ed9ccaa9b095
callmemc <callmemc@gmail.com>
2013-02-26 13:26:47 -0800

    json parsing

--- app/controllers/collect_controller.rb
+++ app/controllers/collect_controller.rb
@@ -1,9 +1,9 @@
 class CollectController < ApplicationController  
   def create
     json_string = params[:updates]
+    puts json_string
     parsed_json = ActiveSupport::JSON.decode(json_string)
-    
-    
+      
     parsed_json.each do |notification|      
       Notification.create(:collectionType => notification[:collectionType], :date => notification[:date], 
       :ownerId => notification[:ownerId], :ownerType => notification[:ownerType], 
