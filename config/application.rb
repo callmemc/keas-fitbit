cdfc830e3c486a62594656301c70513f49bf3e96
callmemc <callmemc@gmail.com>
2013-03-08 15:38:37 -0800

    deleted cache

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ end
 
 module FitBit
   class Application < Rails::Application
+    config.middleware.delete Rack::Cache
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
