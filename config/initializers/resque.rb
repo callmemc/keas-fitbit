0d6f45d166206a0c53fbdce93a5812390398c636
callmemc <callmemc@gmail.com>
2013-03-08 14:29:41 -0800

    debugging

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,4 +3,4 @@ ENV["REDISTOGO_URL"] ||= "redis://redistogo:00b05725a37066e1b63d591bd4ac1326@dor
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
-Dir["/app/app/jobs/*.rb"].each { |file| require file }
+Dir["/app/jobs/*.rb"].each { |file| require file }
