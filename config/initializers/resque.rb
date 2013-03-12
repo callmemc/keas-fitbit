585f4c081a4d56b003a3b0f86945c7af3d1e100c
callmemc <callmemc@gmail.com>
2013-03-12 12:31:08 -0700

    resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,10 @@
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-
 ENV["REDISTOGO_URL"] ||= "redis://redistogo:00b05725a37066e1b63d591bd4ac1326@dory.redistogo.com:10613/"
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
+
+Rails.logger('uri')
+Rails.logger(uri)
+
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
