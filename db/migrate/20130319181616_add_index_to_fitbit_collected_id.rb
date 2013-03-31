3a8cf448a86bcfcf751df0c9e473c21546dc7556
callmemc <callmemc@gmail.com>
2013-03-30 23:50:34 -0700

    remove index migration

--- db/migrate/20130319181616_add_index_to_fitbit_collected_id.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToFitbitCollectedId < ActiveRecord::Migration
-  def change
-    add_index :fb_collected_logs, :logId
-  end
-end
--- db/migrate/20130331060226_remove_index.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIndex < ActiveRecord::Migration
-  def change
-    remove_index :fitbit_collected_ids, :column => :logId
-  end
-end
